e805a62cc2dc58fa1690c7923d9753b68fcff2b9
SpandanaN <Spandana@Spandanas-MacBook-Air.local>
2015-05-22 13:30:52 -0700

    Trip Places with number of Photos

--- app/src/main/java/com/example/traveldiaries/MapsActivity.java
+++ app/src/main/java/com/example/traveldiaries/MapsActivity.java
@@ -1,13 +1,19 @@
 package com.example.traveldiaries;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.traveldiaries.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -17,6 +23,23 @@ public class MapsActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_details);
+        ArrayList<String> trip_places= new ArrayList<String>();
+        trip_places.add("Golden Gate Bridge");
+        trip_places.add("Pier 39");
+        ArrayList<String> trip_photos= new ArrayList<String>();
+        trip_photos.add("5 photos");
+        trip_photos.add("7 photos");
+        ListView listView = (ListView) findViewById(R.id.places);
+        listView.setAdapter(new ListAdapter(this, trip_places, trip_photos));
+        listView.setVisibility(View.VISIBLE);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Intent intent;
+                intent = new Intent(MapsActivity.this, MyPhotos.class);
+                startActivity(intent);
+            }
+        });
         setUpMapIfNeeded();
     }
 
