90cbf70bb62608cd716aba36d175d6082f4c5e1e
SpandanaN <Spandana@Spandanas-MacBook-Air.local>
2015-05-26 12:39:47 -0700

    Photos list and full view with notes

--- app/src/main/java/com/example/traveldiaries/MyPhotos.java
+++ app/src/main/java/com/example/traveldiaries/MyPhotos.java
@@ -1,12 +1,26 @@
 package com.example.traveldiaries;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.traveldiaries.R;
+
+import java.util.ArrayList;
 
 public class MyPhotos extends Activity {
 
@@ -14,6 +28,45 @@ public class MyPhotos extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_photos);
+
+        final ArrayList<Bitmap> pics_icons = new ArrayList<Bitmap>();
+        Bitmap sf = BitmapFactory.decodeResource(getResources(), R.drawable.sf);
+        Bitmap vegas = BitmapFactory.decodeResource(getResources(), R.drawable.vegas);
+        Bitmap sunlight = BitmapFactory.decodeResource(getResources(), R.drawable.sunlight);
+        final Bitmap image1 = BitmapFactory.decodeResource(getResources(), R.drawable.image1);
+        pics_icons.add(sf);
+        pics_icons.add(vegas);
+        pics_icons.add(sunlight);
+        pics_icons.add(image1);
+        final ArrayList<String> pic_notes=new ArrayList<String>();
+        pic_notes.add("IN SF");
+        pic_notes.add("IN VEGAS");
+        pic_notes.add("IT'S HOT");
+        pic_notes.add("KITTY CAT");
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this, pics_icons));
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+       //         Intent intent;
+        //        intent = new Intent(MyPhotos.this, DisplayPhoto.class);
+        //        startActivity(intent);
+                //Dialog image_dialog= new Dialog(MyPhotos.this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+                LayoutInflater inflater =  (LayoutInflater) MyPhotos.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                Dialog image_dialog= new Dialog(MyPhotos.this);
+                image_dialog.setContentView(R.layout.places_pics);
+                //View view1 = inflater.inflate(R.layout.places_pics,null);
+                ImageView image1= (ImageView) image_dialog.findViewById(R.id.Trip_icon);
+                image1.setImageBitmap((Bitmap) pics_icons.get(position));
+                TextView text1=(TextView) image_dialog.findViewById(R.id.trip_name);
+                text1.setText(pic_notes.get(position));
+                //image1.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                text1.setElevation(12);
+                //text1.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                //image_dialog.setContentView(image1);
+                image_dialog.show();
+            }
+        });
+
     }
 
     @Override
