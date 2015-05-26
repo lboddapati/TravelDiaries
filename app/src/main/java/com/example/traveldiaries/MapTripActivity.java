634c5152855ab55e5b6d2b2daf4f2a1b584405db
Lasya Boddapati <lasyaboddapati@gmail.com>
2015-05-26 16:18:47 -0700

    Changed MapTripActivity to save places Json object in parse

--- app/src/main/java/com/example/traveldiaries/MapTripActivity.java
+++ app/src/main/java/com/example/traveldiaries/MapTripActivity.java
@@ -11,6 +11,7 @@ import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
@@ -19,8 +20,10 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,20 +37,60 @@ public class MapTripActivity extends MapActivity {
     //private ArrayList<String> imageObjectIds;
     String tripname = "SomeTrip"; //TODO: Change to actual trip name;
 
+    ArrayList<String> names = new ArrayList<String>();
+    ArrayList<String> address = new ArrayList<String>();
+    ArrayList<LatLng> latLngs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setLayoutFile(R.layout.activity_map_trip);
         super.onCreate(savedInstanceState);
 
         user = ParseUser.getCurrentUser();
-        //imageObjectIds = new ArrayList<String>();
+
+        //TODO: Replace with actual places.
+        latLngs = new ArrayList<LatLng>();
+        latLngs.add(new LatLng(37.258881, -122.032913));
+        latLngs.add(new LatLng(37.270220, -122.015403));
+        latLngs.add(new LatLng(37.291732, -122.032398));
+        latLngs.add(new LatLng(37.287362, -121.944679));
+        latLngs.add(new LatLng(37.240092, -121.960987));
+        latLngs.add(new LatLng(37.281086, -122.026335));
+        latLngs.add(new LatLng(37.260458, -122.029596));
+        latLngs.add(new LatLng(37.296109, -122.029596));
+        latLngs.add(new LatLng(37.325054, -121.867891));
+        latLngs.add(new LatLng(37.285183, -121.939989));
+
+        names = new ArrayList<String>();
+        names.add("origin");
+        names.add("waypoint 1");
+        names.add("waypoint 2");
+        names.add("waypoint 3");
+        names.add("waypoint 4");
+        names.add("waypoint 5");
+        names.add("waypoint 6");
+        names.add("waypoint 7");
+        names.add("waypoint 8");
+        names.add("destination");
+
+        address = new ArrayList<String>();
+        address.add("origin address");
+        address.add("waypoint address 1");
+        address.add("waypoint address 2");
+        address.add("waypoint address 3");
+        address.add("waypoint address 4");
+        address.add("waypoint address 5");
+        address.add("waypoint address 6");
+        address.add("waypoint address 7");
+        address.add("waypoint address 8");
+        address.add("destination address");
 
         final ExpandableListView directionsListView = (ExpandableListView) findViewById(R.id.directions);
-        final Button startTrip = (Button) findViewById(R.id.startTrip);
+        //final Button startTrip = (Button) findViewById(R.id.startTrip);
         final Button finishTrip = (Button) findViewById(R.id.finishTrip);
         final ImageButton addPicture = (ImageButton) findViewById(R.id.addPicture);
 
-        startTrip.setOnClickListener(new View.OnClickListener() {
+        /*startTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startTrip.setVisibility(View.GONE);
@@ -73,7 +116,22 @@ public class MapTripActivity extends MapActivity {
                 }
 
             }
-        });
+        });*/
+
+        trip = new ParseObject("Trip");
+        trip.put("user", user);
+        trip.put("tripName", tripname);
+        try {
+            trip.put("places", getPlacesJSON(names, address, latLngs));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONObject route = getRoute(latLngs);
+        trip.put("route", route);
+        //trip.saveInBackground();
+        trip.pinInBackground();
+        drawRoute(route);
+        displayDirections(route, directionsListView);
 
         addPicture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -150,5 +208,21 @@ public class MapTripActivity extends MapActivity {
         finish();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private JSONObject getPlacesJSON(ArrayList<String> names, ArrayList<String> address, ArrayList<LatLng> latLngs) throws JSONException {
+        JSONObject placesJSON = new JSONObject();
+        JSONArray placesArray = new JSONArray();
+        for (int i=0; i<names.size(); i++) {
+            JSONObject placeDetails = new JSONObject();
+            placeDetails.put("name", names.get(i));
+                placeDetails.put("address", address.get(i));
+                placeDetails.put("latitude", latLngs.get(i).latitude);
+                placeDetails.put("longitude", latLngs.get(i).longitude);
+                placesArray.put(placeDetails);
+        }
+        placesJSON.put("places", placesArray);
+
+        return placesJSON;
     }
 }
