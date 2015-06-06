d9acd8f17a6fb017e0b14c60f56fa86df3660333
Lasya Boddapati <lasyaboddapati@gmail.com>
2015-06-05 23:49:19 -0700

    some minor fixes

--- app/src/main/java/com/example/traveldiaries/ViewTripActivity.java
+++ app/src/main/java/com/example/traveldiaries/ViewTripActivity.java
@@ -256,6 +256,9 @@ public class ViewTripActivity extends FragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_view_trip, menu);
+        if(photoNotes.size() == 0) {
+            menu.findItem(R.id.action_view_all_photos).setVisible(false);
+        }
         return true;
     }
 
